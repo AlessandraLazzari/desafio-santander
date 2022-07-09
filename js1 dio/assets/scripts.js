70249e0bf8e1d0df7fccd6a3c0ad8667da117849
Alessandra <ayumi_kjy@hotmail.com>
2022-07-09 14:46:50 -0300

    organização de pastas

--- js1 dio/assets/scripts.js
+++ js1 dio/assets/scripts.js
@@ -1,5 +1,5 @@
-var currentNumberWrapper = document.getElementById('currentNumber')
-var currentNumber = 0
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
 
 function increment(){
     currentNumber = currentNumber + 1;
